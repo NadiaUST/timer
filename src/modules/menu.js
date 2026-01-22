e92b8507d52820a9371e0eb1e5b2f8c90e89f195
Nadia Ustinova <ustinova7n@gmail.com>
2026-01-22 15:25:32 -0500

    добавлено нажатие на кнопку навигации

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,13 +1,19 @@
 const menu = () => {
   const menuBtn = document.querySelector(".menu");
   const menu = document.querySelector("menu");
+  const closeBtn = menu.querySelector(".close-btn");
+  const menuItems = menu.querySelectorAll("ul > li > a");
 
-  menuBtn.addEventListener("click", () => {
-    if (!menu.style.transform) {
-      menu.style.transform = "translateX(0)";
-    } else {
-      menu.style.transform = "translateX(-100%)";
-    }
-  });
+  const handleMenu = () => {
+    menu.classList.toggle("active-menu");
+  };
+
+  menuBtn.addEventListener("click", handleMenu);
+  closeBtn.addEventListener("click", handleMenu);
+
+  menuItems.forEach((menuItem) =>
+    menuItem.addEventListener("click", handleMenu),
+  );
 };
+
 export default menu;
